93cfaee463612e71869161def3a1d00e53d53e0c
Raiyan <raiyan.appdid@gmail.com>
2023-09-18 17:45:51 +0530

    Updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,29 +4,30 @@ export default defineNuxtConfig({
     public: {
       baseUrl: "https://glosense.in/api/v1",
     },
-    // axios: {
-    //   proxy: true,
-    //   credentials: true,
-    // },
-    // proxy: {
-    //   "/laravel": {
-    //     target: "https://laravel-auth.nuxtjs.app",
-    //     pathRewrite: { "^/laravel": "/" },
-    //   },
-    // },
-    // auth: {
-    //   strategies: {
-    //     laravelSanctum: {
-    //       provider: "laravel/sanctum",
-    //       url: "http://localhost:3000",
-    //     },
-    //   },
-      
-    // },
+    modules: ["@nuxtjs/axios", "@nuxtjs/auth-next"],
+    axios: {
+      proxy: true,
+      credentials: true,
+    },
+    proxy: {
+      "/laravel": {
+        target: "https://laravel-auth.nuxtjs.app",
+        pathRewrite: { "^/laravel": "/" },
+      },
+    },
+    auth: {
+      strategies: {
+        laravelSanctum: {
+          provider: "laravel/sanctum",
+          url: "http://localhost:3000",
+        },
+      },
+    },
   },
-  
+
   devtools: { enabled: false },
   pages: true,
   css: ["@/assets/css/main.css"],
+
   modules: ["@nuxtjs/tailwindcss"],
 });
