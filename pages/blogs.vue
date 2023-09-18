1ecd3067ccc88f4eeed1e8f253ac325e14c6dcb2
Raiyan <raiyan.appdid@gmail.com>
2023-09-18 17:28:23 +0530

    updated

---
+++ pages/blogs.vue
@@ -0,0 +1,10 @@
+<template>
+    <HeaderCommon />
+    <div class="blogs mt-20 sm:mt-24 pb-20 bg-[#efe8df]">
+        <h2 class="py-10 text-secondary text-4xl font-bold text-center">Blogs</h2>
+
+
+
+
+    </div>
+</template>
