5e305d6b0a4d7ab6e585680429d9f66bc25574a2
Raiyan <raiyan.appdid@gmail.com>
2023-09-20 14:48:24 +0530

    updated

---
+++ pages/blogs/index.vue
@@ -0,0 +1,77 @@
+<template>
+    <HeaderCommon />
+
+    <div class="Blog mt-20 sm:mt-24 pb-20 bg-[#efe8df]">
+        <h2 class="py-10 text-secondary text-4xl font-bold text-center">Blogs</h2>
+
+
+        <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 px-3 sm:px-10">
+            <div class="sm:p-10">
+                <div>
+                    <img src="/images/blog-1.png" alt="">
+                </div>
+                <div>
+                    <h2 class="text-2xl sm:text-3xl text-primary font-bold my-3 sm:my-8">WHAT IS PLANT BASED SUPPLEMENTS?
+                    </h2>
+                    <p class="text-xl leading-7">In recent years, the term "plant-based" has taken the health and wellness
+                        world by storm. From plant-based diets to supplements, this trend has gained immense popularity for
+                        all the right reasons......
+                    </p>
+                    <p class="underline text-primary text-xl mt-8"><a href="/blogs/what-is-plant-based-supplements">Read
+                            More</a>
+                    </p>
+                </div>
+            </div>
+            <div class="sm:p-10 mt-8 sm:mt-0">
+                <div>
+                    <img src="/images/blog-1.png" alt="">
+                </div>
+                <div>
+                    <h2 class="text-2xl sm:text-3xl text-primary font-bold my-3 sm:my-8">WHY SLEEP BETTER TO TACKLE PCOS
+                    </h2>
+                    <p class="text-xl leading-7">Did you know that your PCOS symptoms may depend a lot on whether you are
+                        having a
+                        good night’s sleep?
+                        A messed up sleep cycle can mess up your ‘Circadian rhythm’. This...</p>
+                    <p class="underline text-primary text-xl mt-8"><a href="/blogs/what-is-plant-based-supplements">Read
+                            More</a>
+                    </p>
+                </div>
+            </div>
+            <div class="sm:p-10 mt-8 sm:mt-0">
+                <div>
+                    <img src="/images/blog-1.png" alt="">
+                </div>
+                <div>
+                    <h2 class="text-2xl sm:text-3xl text-primary font-bold my-3 sm:my-8">WHY SLEEP BETTER TO TACKLE PCOS
+                    </h2>
+                    <p class="text-xl leading-7">Did you know that your PCOS symptoms may depend a lot on whether you are
+                        having a
+                        good night’s sleep?
+                        A messed up sleep cycle can mess up your ‘Circadian rhythm’. This...</p>
+                    <p class="underline text-primary text-xl mt-8"><a href="/blogs/what-is-plant-based-supplements">Read
+                            More</a>
+                    </p>
+                </div>
+            </div>
+            <div class="sm:p-10 mt-8 sm:mt-0">
+                <div>
+                    <img src="/images/blog-1.png" alt="">
+                </div>
+                <div>
+                    <h2 class="text-2xl sm:text-3xl text-primary font-bold my-3 sm:my-8">WHY SLEEP BETTER TO TACKLE PCOS
+                    </h2>
+                    <p class="text-xl leading-7">Did you know that your PCOS symptoms may depend a lot on whether you are
+                        having a
+                        good night’s sleep?
+                        A messed up sleep cycle can mess up your ‘Circadian rhythm’. This...</p>
+                    <p class="underline text-primary text-xl mt-8"><a href="/blogs/what-is-plant-based-supplements">Read
+                            More</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</template>
---
+++ pages/blogs/what-is-plant-based-supplements.vue
@@ -0,0 +1,95 @@
+<template>
+    <HeaderCommon />
+
+    <div class="Blog Details mt-20 md:mt-24 pb-20 bg-[#efe8df]">
+        <!-- <h2 class="py-10 text-secondary text-4xl font-bold text-center">Blogs</h2> -->
+
+
+
+
+        <div class="container mx-auto md:px-40 px-4 ">
+
+            <nav class="flex py-4" aria-label="Breadcrumb">
+                <ol class="inline-flex items-center space-x-1 md:space-x-3">
+                    <li class="inline-flex items-center">
+                        <a href="/" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600">
+                            <svg class="w-3 h-3 mr-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor" viewBox="0 0 20 20">
+                                <path
+                                    d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                            </svg>
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <div class="flex items-center">
+                            <svg class="w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="m1 9 4-4-4-4" />
+                            </svg>
+                            <a href="/blogs"
+                                class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2">Blogs</a>
+                        </div>
+                    </li>
+                    <li aria-current="page">
+                        <div class="flex items-center">
+                            <svg class="w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="m1 9 4-4-4-4" />
+                            </svg>
+                            <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2">WHAT IS PLANT BASED
+                                SUPPLEMENTS?</span>
+                        </div>
+                    </li>
+                </ol>
+            </nav>
+            <h2 class="text-primary text-3xl md:text-6xl py-10 font-bold">WHAT IS PLANT BASED SUPPLEMENTS?</h2>
+            <img src="/images/blog-1.png" alt="">
+            <p class="my-10 md:text-3xl">In recent years, the term "plant-based" has taken the health and wellness world by
+                storm. From
+                plant-based
+                diets to supplements, this trend has gained immense popularity for all the right reasons.<br />
+                At Glosense, we are passionate about harnessing the incredible benefits of nature to enhance your health and
+                well-being. We here search into the hype surrounding plant-based products and why they deserve a special
+                place in your daily routine.
+            </p>
+            <p class="text-xl md:text-4xl font-bold my-10">Why Plant-Based?</p>
+            <p class="my-10 md:text-3xl">In a world where we are bombarded with options, choosing the right supplements can
+                be overwhelming. So, why should you consider plant-based supplements? The answer lies in the purity and
+                effectiveness of these products.<br /><br />
+                Plant-based supplements are derived from natural sources like fruits, vegetables, herbs, and superfoods.
+                They are free from synthetic chemicals, additives, and artificial ingredients, making them a safe and
+                wholesome choice. When it comes to your hair, which is your crowning glory, opting for plant-based
+                ingredients can make a world of difference.
+            </p>
+            <p class="text-xl md:text-4xl font-bold my-10">Why is it Important to consume Plant-Based Supplements for Your
+                Hair?
+            </p>
+            <p class="my-10 md:text-3xl">-Nutrient-Rich Goodness: Plant-based supplements are packed with essential
+                nutrients like vitamins, minerals, and antioxidants. These nourish your hair follicles, promoting healthy
+                growth and strength.<br /><br />
+                -Reduced Side Effects: Unlike some synthetic supplements, plant-based options are less likely to cause side
+                effects. They work in harmony with your body, ensuring better tolerance and absorption.<br /><br />
+                -Enhanced Hair Health: Ingredients like biotin, collagen, and various superfoods found in plant-based
+                supplements are known to improve hair texture, reduce breakage, and boost shine.<br /><br />
+                -Sustainable Choice: Choosing plant-based supplements also aligns with eco-conscious living. These products
+                typically have a smaller carbon footprint, contributing to a more sustainable planet.
+            </p>
+            <p class="text-xl md:text-4xl font-bold my-10">WHY YOU SHOULD INCORPORATE PLANT BASED SUPPLEMENT??
+            </p>
+            <p class="my-10 md:text-3xl">By now, you might be wondering how you can start benefiting from plant-based
+                supplements. Look no further than our very own Glosense. Our Hair Care Natural Supplement Powder is a
+                testament to the power of plant-based ingredients. Crafted with a blend of superfoods known for their
+                hair-boosting properties, our product is designed to provide your hair with the care it
+                deserves.<br /><br />
+                Unlock the secret to healthy hair with our Hair Care Natural Supplement Powder. Your journey to luscious
+                locks begins here.
+
+            </p>
+        </div>
+
+
+    </div>
+</template>
