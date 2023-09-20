2903ec097a2090bbe9ddf05bb0a94bccb84fa388
Raiyan <raiyan.appdid@gmail.com>
2023-09-20 15:15:33 +0530

    updated

--- pages/contact.vue
+++ pages/contact.vue
@@ -58,17 +58,21 @@
                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path fill="grey"
                                     d="M4 20q-.825 0-1.413-.588T2 18V6q0-.825.588-1.413T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.588 1.413T20 20H4Zm8-7l8-5V6l-8 5l-8-5v2l8 5Z" />
-                            </svg></span><span class="ml-10">myglosense@gmail.com</span></h1>
+                            </svg></span><span class="ml-10"><a
+                                href="mailto:connect@glosense.in">connect@glosense.in</a></span></h1>
                 </div>
                 <div class="social-links flex gap-8">
                     <div class="bg-blue-500 rounded-full w-11 p-2">
-                        <img class="" src="/images/facebook.png" alt="">
+                        <a target="_blank"
+                            href="https://www.facebook.com/profile.php?id=100089037556110&mibextid=LQQJ4d"><img class=""
+                                src="/images/facebook.png" alt=""></a>
                     </div>
                     <div class="bg-[#3b64f8] rounded-full w-11 p-2 my-auto">
-                        <img class="" src="/images/twitter.png" alt="">
+                        <a href=""> <img class="" src="/images/twitter.png" alt=""></a>
                     </div>
                     <div class="bg-[#eca5c2] rounded-full w-11 p-2 my-auto">
-                        <img class="" src="/images/instagram.png" alt="">
+                        <a target="_blank" href="https://instagram.com/glosense_?igshid=YTQwZjQ0NmI0OA=="> <img class=""
+                                src="/images/instagram.png" alt=""></a>
                     </div>
                 </div>
             </div>
