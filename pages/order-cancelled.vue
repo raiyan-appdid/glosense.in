1411dcf19a6a51fb4200ab9ba435b9d2cfa8cd21
Raiyan Memon <raiyan.appdid@gmail.com>
2023-10-07 13:31:57 +0530

    updated

---
+++ pages/order-cancelled.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="h-screen">
+        <h2 class="text-3xl text-center text-red-500 mt-52">Order Cancelled Due to Some Reason! Please Order Again</h2>
+        <h2 class="text-center"><a class=" underline" href="/billing-details">Buy Again</a></h2>
+    </div>
+</template>
+<style></style>
