fd85653990ab6f978c8cadeeeac3e0f21b6bb661
Raiyan Memon <raiyan.appdid@gmail.com>
2023-10-22 21:53:41 +0530

    updated

--- pages/terms-and-conditions.vue
+++ pages/terms-and-conditions.vue
@@ -6,46 +6,54 @@
 
         <div class="px-5">
             <h2 class="text-2xl font-bold">Shipping-</h2>
-            <p>At GLOSENSE, we offer free shipping all over India.</p><br />
-            <p>We only make use of reliable courier companies and/or Speed Post. Glosense is not liable for any delays in
+            <p class="text-justify">At GLOSENSE, we offer free shipping all over India.</p><br />
+            <p class="text-justify">We only make use of reliable courier companies and/or Speed Post. Glosense is not liable
+                for any delays in
                 delivery by the Courier Company/ Speed Post or on account of any factors that cannot be controlled by
                 Glosense. We shall strive to ensure timely delivery of your order within the estimated time as mentioned in
                 the order confirmation.
             </p><br />
-            <p>In the unlikely event that delivery is not possible for any reasons beyond the control of Glosense, including
+            <p class="text-justify">In the unlikely event that delivery is not possible for any reasons beyond the control
+                of Glosense, including
                 but not limited to poor courier services in delivery area, inaccessibility of delivery area, force major
                 events as laid out hereinafter, we shall keep you informed of such inability to deliver and cancel your
                 order, followed by complete refund.
             </p><br />
             <h2 class="text-2xl font-bold">Cancellations-</h2>
-            <p>We will be able to accept a cancellation request any time before we ship your order. In case your credit
+            <p class="text-justify">We will be able to accept a cancellation request any time before we ship your order. In
+                case your credit
                 card/debit card/bank account was already charged for the order, we shall process the refund immediately
                 following cancellation of order, subject to deductions for any banking transaction charges incurred by us
                 pursuant to your order and its cancellation.
             </p><br />
             <h2 class="text-2xl font-bold">Returns Policy-</h2>
-            <p>We are proud that all our shipments go through 2 levels of packaging and 2 levels of inspection before
+            <p class="text-justify">We are proud that all our shipments go through 2 levels of packaging and 2 levels of
+                inspection before
                 dispatch. The likelihood of damage is very small. However, in case there is any damage, here’s how you can
                 get a replacement:
             </p><br />
-            <p>Glosense offers a replacement guarantee for all products ordered online. Products will be replaced only in
+            <p class="text-justify">Glosense offers a replacement guarantee for all products ordered online. Products will
+                be replaced only in
                 case of a manufacturing defect or damage due to transportation.
             </p><br />
-            <p>If you are eligible for a replacement, you may notify us within 2 days from the date of delivery.
+            <p class="text-justify">If you are eligible for a replacement, you may notify us within 2 days from the date of
+                delivery.
             </p><br />
-            <p>Before confirming an exchange, we would require proof of defect in product in the form of a photo/video,
+            <p class="text-justify">Before confirming an exchange, we would require proof of defect in product in the form
+                of a photo/video,
                 while the product is still in its packaging.
             </p><br />
-            <p>In case the product you have returned is out of stock, we will provide you with a credit note equal to the
+            <p class="text-justify">In case the product you have returned is out of stock, we will provide you with a credit
+                note equal to the
                 value of the product you purchased. The credit note will be sent to you via email and can be used to
                 purchase anything of that value from the online store within three months.</p><br />
             <h2 class="text-2xl font-bold">Currency Transactions-</h2>
-            <p>All transactions are processed in Indian Rupees (INR).
+            <p class="text-justify">All transactions are processed in Indian Rupees (INR).
             </p><br />
-            <h2 class="text-2xl font-bold">Processing Orders-</h2>
-            <p>Our business hours are 10 am to 6 pm Indian Standard Time (IST), Monday to Friday. If you need to contact us
-                outside our business hours, please email us at connect@glosense.in and we will respond as soon as possible.
-            </p>
-        </div>
+        <h2 class="text-2xl font-bold">Processing Orders-</h2>
+        <p class="text-justify">Our business hours are 10 am to 6 pm Indian Standard Time (IST), Monday to Friday. If
+            you need to contact us
+            outside our business hours, please email us at connect@glosense.in and we will respond as soon as possible.
+        </p>
     </div>
-</template>
+</div></template>
