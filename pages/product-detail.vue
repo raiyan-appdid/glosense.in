e7fae21412687a29fd0220bbb415c678140731ff
Raiyan <raiyan.appdid@gmail.com>
2023-11-07 12:28:16 +0530

    updated

--- pages/product-detail.vue
+++ pages/product-detail.vue
@@ -1,4 +1,40 @@
 <script setup>
+
+
+const facebookPixelCode = `
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1432488057327276');
+    fbq('track', 'PageView');
+`;
+
+const noscriptCode = `
+    <img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=1432488057327276&ev=PageView&noscript=1"
+    />
+`;
+
+const headData = {
+    script: [
+        {
+            innerHTML: facebookPixelCode
+        }
+    ],
+    noscript: [
+        {
+            innerHTML: noscriptCode
+        }
+    ]
+};
+
+
+
 const token = useCookie('token');
 const count = ref(1);
 const counter = useCounter()
