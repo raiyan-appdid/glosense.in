2da7955265f6327d92c941a44a5d7a97be6c54ea
Raiyan <raiyan.appdid@gmail.com>
2023-11-30 16:31:17 +0530

    updated

--- pages/my-order.vue
+++ pages/my-order.vue
@@ -120,7 +120,7 @@ onMounted(() => {
                         </div>
                     </div>
                     <div
-                        class="flex justify-center flex-col md:flex-row flex-col items-stretch w-full space-y-4 md:space-y-0 md:space-x-6 xl:space-x-8">
+                        class="flex justify-center md:flex-row flex-col items-stretch w-full space-y-4 md:space-y-0 md:space-x-6 xl:space-x-8">
                         <div class="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50  space-y-6">
                             <h3 class="text-xl  font-semibold leading-5 text-gray-800">Summary</h3>
                             <div
