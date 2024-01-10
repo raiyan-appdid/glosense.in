34d4538174db7efc46bbce8a66f86d4a04e9e8a1
Raiyan <raiyan.appdid@gmail.com>
2024-01-10 18:22:43 +0530

    updated

--- pages/product-detail.vue
+++ pages/product-detail.vue
@@ -230,12 +230,12 @@ async function getUser() {
                     </div>
                     <div class="flex my-0">
 
-                        <span v-for="index in parseInt(globalStar)"><svg width="45" height="45" viewBox="0 0 24 24"
+                        <span v-for="index in parseInt(globalStar)"><svg width="25" height="25" viewBox="0 0 24 24"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path fill="#eab508"
                                     d="m5.825 22l1.625-7.025L2 10.25l7.2-.625L12 3l2.8 6.625l7.2.625l-5.45 4.725L18.175 22L12 18.275L5.825 22Z" />
                             </svg></span>
-                        <span v-for="index1 in parseInt(5 - globalStar)"><svg width="45" height="45" viewBox="0 0 24 24"
+                        <span v-for="index1 in parseInt(5 - globalStar)"><svg width="25" height="25" viewBox="0 0 24 24"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path fill="white"
                                     d="m5.825 22l1.625-7.025L2 10.25l7.2-.625L12 3l2.8 6.625l7.2.625l-5.45 4.725L18.175 22L12 18.275L5.825 22Z" />
