940b845db5e58de911fbe02d58335557217352a6
Raiyan <raiyan.appdid@gmail.com>
2024-01-24 18:17:05 +0530

    updated

--- components/HeaderCommon.vue
+++ components/HeaderCommon.vue
@@ -107,6 +107,14 @@ onUnmounted(() => {
               Register
             </button></a>
         </div>
+        <a href="/product-detail"><span class="text-sm sm:hidden font-semibold text-black"><svg width="20" height="20"
+              viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="10" cy="28" r="2" fill="#43a190" />
+              <circle cx="24" cy="28" r="2" fill="#43a190" />
+              <path fill="#43a190"
+                d="M4.98 2.804A1 1 0 0 0 4 2H0v2h3.18l3.84 19.196A1 1 0 0 0 8 24h18v-2H8.82l-.8-4H26a1 1 0 0 0 .976-.783L29.244 7h-2.047l-1.999 9H7.62Z" />
+              <path fill="#43a190" d="M21.586 6.586L18 10.172V2h-2v8.172l-3.586-3.586L11 8l6 6l6-6z" />
+            </svg></span></a>
       </div>
 
       <!-- drawer component -->
