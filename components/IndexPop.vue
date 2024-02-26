ac804ae0b8d3db4d42a8a5ee079137a94472fcf4
Raiyan <raiyan.appdid@gmail.com>
2024-02-26 17:01:17 +0530

    updated

--- components/IndexPop.vue
+++ components/IndexPop.vue
@@ -7,6 +7,7 @@ const apiUrl = config.public.baseUrl;
 const nuxtApp = useNuxtApp();
 const emailGiven = useCookie('email-given');
 import Notiflix from 'notiflix';
+const downloadButtonShow = ref(false)
 
 function closeModal() {
     let modal = document.getElementById('index-pop-modal')
@@ -55,9 +56,10 @@ async function storeEmail() {
         //         dynamicImage.value = response._data.image;
         //     },
         // })
+        downloadButtonShow.value = true;
         emailGiven.value = 'given';
         Notiflix.Notify.success("Thank you");
-        closeModal();
+        // closeModal();
     }
 }
 
@@ -106,8 +108,15 @@ async function storeEmail() {
 
 
                             </div>
-                            <a href="https://admin.glosense.in/site/assets/pdf/E-book-Hair-Fall-final.pdf"
-                                class="text-center"> <button download="E-book-Hair-Fall">Download Now</button></a>
+                            <a v-show="downloadButtonShow" href="/pdf/E-book-Hair-Fall-final.pdf"
+                                download="E-Book-Hair-fall" class="text-center mt-3">
+                                <button download="E-book-Hair-Fall"><svg xmlns="http://www.w3.org/2000/svg" width="40"
+                                        height="40" viewBox="0 0 16 16">
+                                        <path fill="#4fa48c"
+                                            d="m9 9.114l1.85-1.943a.52.52 0 0 1 .77 0c.214.228.214.6 0 .829l-1.95 2.05a1.552 1.552 0 0 1-2.31 0L5.41 8a.617.617 0 0 1 0-.829a.52.52 0 0 1 .77 0L8 9.082V.556C8 .249 8.224 0 8.5 0s.5.249.5.556z" />
+                                        <path fill="#4fa48c"
+                                            d="M16 13.006V10h-1v3.006a.995.995 0 0 1-.994.994H3.01a.995.995 0 0 1-.994-.994V10h-1v3.006c0 1.1.892 1.994 1.994 1.994h10.996c1.1 0 1.994-.893 1.994-1.994" />
+                                    </svg></button></a>
                         </div>
                     </div>
                 </div>
