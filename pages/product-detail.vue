a181216fb4f63e4570bd39a2070bcce84a75b8c9
Raiyan <raiyan.appdid@gmail.com>
2024-03-19 15:14:07 +0530

    updated

--- pages/product-detail.vue
+++ pages/product-detail.vue
@@ -673,7 +673,7 @@ function generateAvatarUrl(title) {
             </div> -->
         </div>
 
-        <div class="container-fluid my-8">
+        <!-- <div class="container-fluid my-8">
             <svg data-v-6da3ec0c="" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
                 preserveAspectRatio="none" class="w-full" style="height: 69px;">
                 <path data-v-6da3ec0c=""
@@ -711,7 +711,7 @@ function generateAvatarUrl(title) {
                     d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
                     class="shape-fill" fill="#42a08f"></path>
             </svg>
-        </div>
+        </div> -->
 
         <div class="product-review">
             <div class="container mx-auto">
